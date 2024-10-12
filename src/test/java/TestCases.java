7d291c0a1ab41034229da0af8cfdd49e4c37eb8f
Ashishlearning1026 <47783109+Ashishlearning1026@users.noreply.github.com>
2024-10-13 00:11:31 +0530

    Add files via upload

---
+++ src/test/java/TestCases.java
@@ -0,0 +1,33 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCases {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    @Test
+    public void testAdd() {
+        int total = 8;
+        int sum = add(4, 4);
+        Assert.assertEquals(sum, total);
+    }
+
+    @Test
+    public void testFailedAdd() {
+        int total = 9;
+        int sum = add(10, 5);
+        Assert.assertEquals(sum, total);
+    }
+
+    @Test
+    public void testSub() {
+        int total = 0;
+        int sub = sub(4, 4);
+        Assert.assertEquals(sub, total);
+    }
+}
